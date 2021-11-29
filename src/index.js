56704f49dc9468042fbce5555feff798f67999b0
Shudhanshu Ranjan <sudhanshu03062002@gmail.com>
2021-11-29 16:12:55 +0530

    Progress made

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
-
+const socketio = require('socket.io');
 const app = express();
-
+const server = http.createServer(app);
+const io = socketio(server)
 const port  = process.env.PORT || 3000
 const publicPath = path.join(__dirname,'../public');
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+let count = 0;
+io.on('connection',(socket)=>{
+   console.log('New Connection');
+   
+   socket.emit('countupdated',count);
+
+   socket.on('increment',()=>{
+       count++;
+       io.emit('countupdated',count);
+   })
+})
+server.listen(port,()=>{
     console.log(`Server is running on port: ${port}`)
 })
-
-
-
-
